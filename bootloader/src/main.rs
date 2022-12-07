f214c763dc4080f8dffd91d17c33783b3deb429a
Patrick Roy <patrick24r@gmail.com>
2022-12-07 06:48:24 -0500

    Linker issues with RTIC, but almost there

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -2,45 +2,87 @@
 #![no_main]
 #![no_std]
 
-use cortex_m_rt::entry;
-use stm32h7xx_hal::{prelude::*, stm32};
+use panic_rtt_target as _;
+use rtic::app;
+use stm32h7xx_hal::gpio::{gpiob::PB0, gpiob::PB14, gpioe::PE1, Output, PushPull};
+use stm32h7xx_hal::prelude::*;
+use systick_monotonic::{fugit::Duration, Systick};
 
-#[allow(unused_imports)]
-use panic_itm;
+#[app(device = stm32h7xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
+mod app {
+    use super::*;
 
-#[entry]
-fn main() -> ! {
-    let dp = stm32::Peripherals::take().unwrap();
-    let cp = cortex_m::Peripherals::take().unwrap();
+    #[shared]
+    struct Shared {}
 
-    // Constrain power
-    let power = dp.PWR.constrain();
-    let power_config = power.freeze();
-    let rcc = dp.RCC.constrain();
+    #[local]
+    struct Local {
+        led_1: PB0<Output<PushPull>>,
+        led_2: PE1<Output<PushPull>>,
+        led_3: PB14<Output<PushPull>>,
+    }
 
-    let ccdr = rcc
-        .use_hse(8.MHz())
-        .sys_ck(120.MHz())
-        .sysclk(120.MHz())
-        .pclk1(24.MHz())
-        .pclk2(24.MHz())
-        .freeze(power_config, &dp.SYSCFG);
+    #[monotonic(binds = SysTick, default = true)]
+    type MonoTimer = Systick<1000>;
 
-    let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
-    let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
+    #[init]
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        // Setup clocks
+        let power_config = cx.device.PWR.constrain().freeze();
+        let rcc = cx.device.RCC.constrain();
 
-    let mut led_1 = gpiob.pb0.into_push_pull_output();
-    let mut led_2 = gpioe.pe1.into_push_pull_output();
-    let mut led_3 = gpiob.pb14.into_push_pull_output();
+        let mono = Systick::new(cx.core.SYST, 120_000_000);
+        let _clocks = rcc
+            .use_hse(8.MHz())
+            .sysclk(120.MHz())
+            .pclk1(36.MHz())
+            .freeze(power_config, &cx.device.SYSCFG);
 
-    let mut delay = cp.SYST.delay(ccdr.clocks);
+        // Setup LEDs
+        let gpiob = cx.device.GPIOB.split(_clocks.peripheral.GPIOB);
+        let gpioe = cx.device.GPIOE.split(_clocks.peripheral.GPIOE);
 
-    loop {
-        led_1.toggle();
-        delay.delay_ms(300_u16);
-        led_2.toggle();
-        delay.delay_ms(300_u16);
-        led_3.toggle();
-        delay.delay_ms(300_u16);
+        let led_1 = gpiob.pb0.into_push_pull_output();
+        let led_2 = gpioe.pe1.into_push_pull_output();
+        let led_3 = gpiob.pb14.into_push_pull_output();
+
+        // Schedule the blinking tasks
+        blink_1::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+        blink_2::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+        blink_3::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+
+        (
+            Shared {},
+            Local {
+                led_1,
+                led_2,
+                led_3,
+            },
+            init::Monotonics(mono),
+        )
+    }
+
+    #[task(local = [led_1])]
+    fn blink_1(cx: blink_1::Context) {
+        cx.local.led_1.toggle();
+
+        // Blink again later
+        blink_1::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+    }
+
+    #[task(local = [led_2])]
+    fn blink_2(cx: blink_2::Context) {
+        cx.local.led_2.toggle();
+
+        // Blink again later
+        blink_2::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+    }
+
+    #[task(local = [led_3])]
+    fn blink_3(cx: blink_3::Context) {
+        cx.local.led_3.toggle();
+
+        // Blink again later
+        blink_3::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
     }
 }
